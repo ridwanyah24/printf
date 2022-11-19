c1d5e52547a05b3ee0252326d9503a49ff9666a5
ridwanyah24 <ridwangames24@gmail.com>
2022-11-19 18:26:41 +0000

    n

--- print_str.c
+++ print_str.c
@@ -8,10 +8,19 @@ int print_str(va_list args)
 {
 	char *string;
 	int j;
+	char null[] = "(null)";
+	string = va_arg(args, char *);
 
+	if (string == NULL)
+	{
+		for(i = 0; null[i] != '\0'; i++)
+		{
+			_putchar(null[i]);
+		}
+		return (100);
+	}
 	if(args)
 	{
-		string = va_arg(args, char*);
 		for (j = 0; string[j] != '\0'; j++)
 		{
 			_putchar(string[j]);
